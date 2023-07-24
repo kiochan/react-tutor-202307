987143082ddf02834185704c153d35a64a6acf6b
YueMeiHong <YueMeiHongMoko@gmail.com>
2023-07-24 11:00:01 -0400

    c06 all

---
+++ project/moko/src/components/Pages/home.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const HomePage= () =>{
+    return <p className="rootpagestitle">大家好，我是月妹红moko，可以叫我妹红/蘑菇/moko，喜欢跟人贴贴</p>
+}
---
+++ project/moko/src/components/Pages/photopage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const photopage= () =>{
+    return <>
+        <img src="images/今天的贴贴.png" /><br />
+        <img src="images/夙愿酱.png" /><br />
+        <img src="images/舞台猫娘.png" /><br />
+        <img src="images/集合.png" /><br />
+    </>
+}
