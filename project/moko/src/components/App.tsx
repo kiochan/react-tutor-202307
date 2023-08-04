4f79ea60db1e41444b4980e17b3a213ad7a34aaf
YueMeiHong <YueMeiHongMoko@gmail.com>
2023-08-03 23:30:30 -0400

    创建router

--- project/moko/src/components/App.tsx
+++ project/moko/src/components/App.tsx
@@ -1,10 +1,19 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import { Nav } from "./Nav"
-import { PageComponents } from "./pages"
-import { PageId } from "../setting"
+import { PageComponents } from "./Pages"
+import { PageId, allPages } from "../setting"
 
 export const App = () => {
-    const [pageId, setpageId] = useState<PageId>("index")
+    let pageIdDefault: PageId =  window.location.hash.replace(/^#/,'') as PageId;
+    if (!allPages.includes(pageIdDefault)) {
+        pageIdDefault = allPages[0];
+    }
+    const [pageId, setpageId] = useState<PageId>(pageIdDefault)   
+
+    useEffect(() =>{
+        window.location.hash = pageId;
+    },[pageId])
+    
     const PageComponent = PageComponents[pageId]
 
     return <>
