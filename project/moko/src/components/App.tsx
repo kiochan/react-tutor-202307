6c44cb6f947aa43e267d3d78b973bdc4b1d5c3a5
YueMeiHong <YueMeiHongMoko@gmail.com>
2023-08-03 23:35:48 -0400

    现在返回按钮可以用了

--- project/moko/src/components/App.tsx
+++ project/moko/src/components/App.tsx
@@ -11,9 +11,18 @@ export const App = () => {
     const [pageId, setpageId] = useState<PageId>(pageIdDefault)   
 
     useEffect(() =>{
+        window.addEventListener("popstate",function(event){
+            let pageIdDefault: PageId =  window.location.hash.replace(/^#/,'') as PageId;
+            if (!allPages.includes(pageIdDefault)) {
+                pageIdDefault = allPages[0];
+            } 
+            setpageId(pageIdDefault)
+        },false);
+    },[])
+    
+    useEffect(() =>{
         window.location.hash = pageId;
     },[pageId])
-    
     const PageComponent = PageComponents[pageId]
 
     return <>
