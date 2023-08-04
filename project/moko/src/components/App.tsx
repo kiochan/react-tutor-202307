77413e82aa10aaf41b34faf0abadfa023d59d1dd
YueMeiHong <YueMeiHongMoko@gmail.com>
2023-08-04 00:11:06 -0400

    乱写url会回到主页

--- project/moko/src/components/App.tsx
+++ project/moko/src/components/App.tsx
@@ -1,32 +1,56 @@
-import React, { useEffect, useState } from "react"
+import React, { useCallback, useEffect, useState } from "react"
 import { Nav } from "./Nav"
 import { PageComponents } from "./Pages"
 import { PageId, allPages } from "../setting"
 
-export const App = () => {
-    let pageIdDefault: PageId =  window.location.hash.replace(/^#/,'') as PageId;
-    if (!allPages.includes(pageIdDefault)) {
-        pageIdDefault = allPages[0];
-    }
-    const [pageId, setpageId] = useState<PageId>(pageIdDefault)   
+function useRouter():[string, (newState:string) => void] {
+    const original: string =  window.location.hash.replace(/^#/,'') 
+    const [hash, setHash] = useState(original)
+
 
     useEffect(() =>{
         window.addEventListener("popstate",function(event){
-            let pageIdDefault: PageId =  window.location.hash.replace(/^#/,'') as PageId;
-            if (!allPages.includes(pageIdDefault)) {
-                pageIdDefault = allPages[0];
-            } 
-            setpageId(pageIdDefault)
+            const hash= window.location.hash.replace(/^#/,'') 
+            setHash(hash)
         },false);
     },[])
+
+    return [
+        hash,
+        useCallback(
+            function (newState:string){
+            window.location.hash = newState
+            setHash(newState)
+            },[window.location.hash,setHash])
+    ]
+}
+
+function returnHomeIfNotInPages(pageId:string):PageId{
+    if(!allPages.includes(pageId as PageId)){
+        return allPages[0]
+    }
+    return pageId as PageId
+}
+
+export const App = () => {
+    const [hash, setHash] = useRouter()
     
+    const pageId = returnHomeIfNotInPages(hash)
+
     useEffect(() =>{
-        window.location.hash = pageId;
-    },[pageId])
-    const PageComponent = PageComponents[pageId]
+        window.addEventListener("popstate",function(event){
+            let pageIdDefault:PageId = window.location.hash.replace(/^#/,'') as PageId
+            if (!allPages.includes(pageIdDefault)){
+                pageIdDefault=allPages[0]
+            }
+            setHash(pageIdDefault)
+        },false);
+    },[])
+
+    const PageComponent = PageComponents[pageId];
 
     return <>
-        <Nav pageId={pageId} onPageChange={setpageId}></Nav>
+        <Nav pageId={pageId} onPageChange={setHash}></Nav>
         <PageComponent />
     </>
 }
