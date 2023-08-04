123099d091a33e84e0a7a8421b1f49099cab8a69
YueMeiHong <YueMeiHongMoko@gmail.com>
2023-08-04 00:23:51 -0400

    修改app保持整洁

--- project/moko/src/components/App.tsx
+++ project/moko/src/components/App.tsx
@@ -1,51 +1,15 @@
-import React, { useCallback, useEffect, useState } from "react"
+import React from "react"
 import { Nav } from "./Nav"
 import { PageComponents } from "./Pages"
 import { PageId, allPages } from "../setting"
+import { useRouter } from "./hooks/useRouter"
+import { returnHomeIfNotInPages } from "./utils/aa"
 
-function useRouter():[string, (newState:string) => void] {
-    const original: string =  window.location.hash.replace(/^#/,'') 
-    const [hash, setHash] = useState(original)
-
-
-    useEffect(() =>{
-        window.addEventListener("popstate",function(event){
-            const hash= window.location.hash.replace(/^#/,'') 
-            setHash(hash)
-        },false);
-    },[])
-
-    return [
-        hash,
-        useCallback(
-            function (newState:string){
-            window.location.hash = newState
-            setHash(newState)
-            },[window.location.hash,setHash])
-    ]
-}
-
-function returnHomeIfNotInPages(pageId:string):PageId{
-    if(!allPages.includes(pageId as PageId)){
-        return allPages[0]
-    }
-    return pageId as PageId
-}
 
 export const App = () => {
     const [hash, setHash] = useRouter()
-    
     const pageId = returnHomeIfNotInPages(hash)
 
-    useEffect(() =>{
-        window.addEventListener("popstate",function(event){
-            let pageIdDefault:PageId = window.location.hash.replace(/^#/,'') as PageId
-            if (!allPages.includes(pageIdDefault)){
-                pageIdDefault=allPages[0]
-            }
-            setHash(pageIdDefault)
-        },false);
-    },[])
 
     const PageComponent = PageComponents[pageId];
 
@@ -54,6 +18,7 @@ export const App = () => {
         <PageComponent />
     </>
 }
+
 
 
 
